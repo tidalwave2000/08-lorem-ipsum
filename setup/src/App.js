e25629ee50fa74181e686db840ddb93b2d7fbb3c
Pierce <ppuckette@gmail.com>
2022-07-11 16:44:59 -0400

    setup up form for lorem ipsum

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,11 +1,45 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("hello world");
+	};
+
+	return (
+		<section className="section-center">
+			<h3>tired of boring lorem ipsum?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="amount">paragraph:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button type="submit" className="btn">
+					generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+					doloribus sed itaque nobis delectus ullam quisquam totam ducimus
+					corporis earum repellat, neque corrupti iure consectetur inventore
+					repellendus veniam odit ea.
+				</p>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+					doloribus sed itaque nobis delectus ullam quisquam totam ducimus
+					corporis earum repellat, neque corrupti iure consectetur inventore
+					repellendus veniam odit ea.
+				</p>
+			</article>
+		</section>
+	);
 }
 
 export default App;
